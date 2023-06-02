ae6b1feccbc5a163f0ec884349c6ba55da245c49
Coding-Ocean <eijo524@gmail.com>
2023-06-02 10:51:48 +0900

    プロジェクト ファイルを追加します。

---
+++ ascii.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+void hitagi_ascii() 
+{
+    cout << "                     " << endl;
+    cout << "Å@Å@Å@Å^ÅPÅPÅPÅ_     " << endl;
+    cout << "Å@Å@Å^Å@Å@Å@ò§Å@Å_   " << endl;
+    cout << "Å@ /Å@|§Å@Åb | Å_ ò§ " << endl;
+    cout << "Å@/Å@||ò§ Åb | |Åb | " << endl;
+    cout << "Å^ / ò∏∆ Å_L_ ∆ NÅ@ò§" << endl;
+    cout << "ÇVÅb KÅúò§Å@®Åú>/ | |" << endl;
+    cout << "ÅbÅbÅbÅPÅ@Å@ ÅP/ /|Çm" << endl;
+    cout << "`⁄É©_>Å@Å@ÅMÅ@/_/ Çu " << endl;
+    cout << "Å@Åb |Å_Å@Å‹Å@Å^Å@ | " << endl;
+    cout << "Å@Åb | |)`Å[ÉCò§_Å@| " << endl;
+    cout << "Å@/Å^| |ò§ÅQ/Å@| Å_| " << endl;
+    cout << "`// ÉmÉm_|_/ ÅQ||    " << endl;
+}
+void hanekawa_ascii() 
+{
+    cout << "Å@Å@Å@ÅQÅQÅQÅQ       " << endl;
+    cout << "Å@ _Å^Å@Å@Å@Å@Å_     " << endl;
+    cout << "Å@/Å@ ∏Å@Å@Å@Å@ ò§   " << endl;
+    cout << "`/ / Å»Å@ò§Å@ ò§ ò§  " << endl;
+    cout << "ÅbÅb/Å[Å_Å@Å_Å@| ||  " << endl;
+    cout << "Åb ⁄r=ò§ Å_ÅÜÅ_õô||  " << endl;
+    cout << "ÅbÅbÀO…)^( ÀO…/ /)|  " << endl;
+    cout << "|Å_>Å[ÅL§ÅMÅ[/ÉC  ò§ " << endl;
+    cout << "ò§Å@Å_Å@Å[Å@ Å^Å@Å@| " << endl;
+    cout << "ÅÑ>Å@ ò§Å[ ÉCÉwÅ@_Ém " << endl;
+    cout << "Åi ÅQÅ^|_ ÅQ/ (Å@…ÅÉ " << endl;
+    cout << "`Å^ /Å@|_ÅQ/Å@( ))Å_ " << endl;
+    cout << "ÅbÅ@ÅÑ ò§ / ÅÉ( ø) Åb" << endl;
+    cout << "/Å@/Å_Å^[ÃÅ_Å^ÅÑÅÉ Åb" << endl;
+    cout << "ÉwÅb <Å^/|Å_> (  )Éw)" << endl;
+    cout << "L_Å» Åq/(|£Å@Å@)…_Å^ " << endl;
+    cout << "Åb ÅbÅ@ ÅbÅ@Å@/Å@/   " << endl;
+}
+void kanbaru_ascii() 
+{
+    cout << "                     " << endl;
+    cout << "Å@Å@Å@Å^ÅPÅPÅPÅ_     " << endl;
+    cout << "Å@Å@Å^Å@Å@Å@Å@Å@Å_   " << endl;
+    cout << "Å@ //Å@Å@Å@/|ÅbÅ@ ò§ " << endl;
+    cout << "Å@//Å@Å@/ /_|Å»Å@Å@| " << endl;
+    cout << "`/ ⁄≤ÉÄ/Å^ r=<| Éw | " << endl;
+    cout << "/ÉC |ÀO)Å@ ÀO…|/ø… | " << endl;
+    cout << "Å@⁄   ÅqÅ@Å@Å@ ÉC ò∏ " << endl;
+    cout << "Å@ )êl Å[Å\Åå / ⁄/   " << endl;
+    cout << "Å@Å@ ⁄Å_ÅQÅ@ÉCò§(    " << endl;
+    cout << "Å@Å@Å@Å@Å» Å^ Å^Å_   " << endl;
+    cout << "Å@Å@Å@ / |/Å@ÅbÅ@ ò§ " << endl;
+    cout << "Å@Å@ Å^Å@/_ÅQò´Å^Å@| " << endl;
+    cout << "Å@Å@ ÅÑ ë¸Å_Å@/Å@Å@| " << endl;
+    cout << "Å@Å@ Å_</LÇu Å»Å^ò§| " << endl;
+}
+void hatikuji_ascii() 
+{
+    cout << "                     " << endl;
+    cout << "Å@ Å»/L_ÅQÅQÅQ       " << endl;
+    cout << "Å_/ÅÑÅ\Å\Å\Å\§Å_Å@/L_" << endl;
+    cout << "Å_⁄≤Å»/≤êl⁄ÅSÉw Çu/Å^" << endl;
+    cout << "Å@Çu_Å_Å@ uÅ^_ L)ÇxÅP" << endl;
+    cout << "Å@/⁄Åù) uÅ@(Åù) >ÅbÅP" << endl;
+    cout << "Å@Çx///Å@Å@////(/Å‹i " << endl;
+    cout << "Å@(ÅQÅQÅQÅQÅQÅ@Å@_Ém " << endl;
+    cout << "Å@Å@Å@Å@(Çx) )Å@…    " << endl;
+    cout << "Å@ ÅºÉjÉjÉjÉj_Å⁄_Å^ÅP" << endl;
+    cout << "ÉmÅ‹ÅÉÅQ)Å@(ÅQÅQÉw   " << endl;
+    cout << "|Å@ Å^À Å_Å^ ) |Å@ò§ " << endl;
+    cout << "ò§ / /ôºë¸∆Ét| |Å@ | " << endl;
+}
---
+++ ascii.h
@@ -0,0 +1,5 @@
+#pragma once
+void hitagi_ascii();
+void hanekawa_ascii();
+void kanbaru_ascii();
+void hatikuji_ascii();
